bde6941546a587d57e8616054fb71fef024e2837
Sébastien Villemain <39873273+Tr4ncer@users.noreply.github.com>
2012-09-16 15:32:06 +0000

    Last version in this repository...

---
+++ README.txt
@@ -0,0 +1,17 @@
+TR Engine
+-> http://sourceforge.net/projects/trengine/
+
+This project is executed with PHP 5.4, NetBeans IDE 7.2.
+Get it here: http://www.php.com / http://netbeans.org/
+
+Subversion Access
+-> svn checkout svn://svn.code.sf.net/p/trengine/code/trunk trengine-code
+
+Thank you for your interest,
+cordially,
+Sébastien Villemain (the-trancer / trancer)
+
+Author - Sébastien Villemain
+SourceForge : http://sourceforge.net/users/the-trancer
+Website : http://www.trancer-studio.net
+Contact : the-trancer@users.sourceforge.net
--- modules/contact/index.module.php
+++ modules/contact/index.module.php
@@ -5,12 +5,12 @@ if (!defined("TR_ENGINE_INDEX")) {
 }
 
 class Module_Contact_Index extends Module_Model {
-	
+
 	public function display() {
-		echo "Si vous souhaitez me contacter, le plus simple est de m'envoyer un e-mail (<a href=\"mailto:villemain.sebastien@hotmail.fr\">ici</a>)"
+		echo "Si vous souhaitez me contacter, le plus simple est de m'envoyer un e-mail (<a href=\"mailto:villemain.s_AT_gmail.com\">gmail.com</a>)"
 		. "<br />Retrouvez mes coordonn&#233;es compl&#232;te sur <a href=\"?mod=profilcv\">mon profil</a>.";
 	}
-	
+
 	public function setting() {
 		return "Pas de setting...";
 	}
