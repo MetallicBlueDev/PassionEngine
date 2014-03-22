54d083acba1767de90eb8a4aaac852d48854c84f
Sébastien Villemain <39873273+Tr4ncer@users.noreply.github.com>
2014-03-22 19:27:30 +0000

    syntaxe

--- includes/js/javascriptenabled.js
+++ includes/js/javascriptenabled.js
@@ -1,6 +1,14 @@
 /**
- * D�tection du javascript via cookie
- * 
- * @author Sebastien Villemain
+ * Détection du javascript via cookie
+ *
+ * @author Sébastien Villemain
  */
-function javascriptEnabled(name){if(navigator.cookieEnabled){var cookie_name=name;if(document.cookie.indexOf(cookie_name+'=')<0){document.cookie=cookie_name+'='+escape(1);document.location.reload()}}}
+function javascriptEnabled(name) {
+    if (navigator.cookieEnabled) {
+        var cookie_name = name;
+        if (document.cookie.indexOf(cookie_name + '=') < 0) {
+            document.cookie = cookie_name + '=' + escape(1);
+            document.location.reload()
+        }
+    }
+}
