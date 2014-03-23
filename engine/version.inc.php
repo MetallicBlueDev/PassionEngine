97b4549a9dfd44046fd679d090f16ff301f329b7
Sébastien Villemain <39873273+Tr4ncer@users.noreply.github.com>
2014-03-23 12:09:59 +0000

    On va booster le numéro...

--- engine/version.inc.php
+++ engine/version.inc.php
@@ -1,21 +1,19 @@
 <?php
-
 if (!defined("TR_ENGINE_INDEX")) {
-	require("core/secure.class.php");
-	new Core_Secure();
+    require("core/secure.class.php");
+    new Core_Secure();
 }
 
 /**
  * Numéro de version du moteur
- * 
+ *
  * Controle de révision
  * xx -> version courante
  * xx -> fonctionnalitées ajoutées
  * xx -> bugs ou failles critiques corrigés
  * xx -> bug mineur
- * 
+ *
  * @var String
  */
-$TR_ENGINE_VERSION = "0.0.0.0";
-
+$TR_ENGINE_VERSION = "1.1.0.0";
 ?>
