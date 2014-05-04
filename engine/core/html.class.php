6cd1c85bd4a793a8a376943a9fee3c4d87721fd5
Sébastien Villemain <39873273+Tr4ncer@users.noreply.github.com>
2014-05-04 09:14:23 +0000

    Ne pas faire de test javascript si la sécurité est verrouillée

--- engine/core/html.class.php
+++ engine/core/html.class.php
@@ -493,7 +493,7 @@ class Core_Html {
 
             // Lorsque l'on ne force pas l'inclusion on fait un nouveau test
             if (!$forceIncludes) {
-                if (!$this->javascriptEnabled()) {
+                if (!$this->javascriptEnabled() && !Core_Secure::getInstance()->locked()) {
                     $this->addJavascriptFile("javascriptenabled.js");
                     $this->addJavascriptCode("javascriptEnabled('" . $this->cookieTestName . "');");
                 }
