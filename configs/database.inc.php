7e8fc42e46aae8988c18714a8d54008f3c3314ea
Sébastien Villemain <39873273+Tr4ncer@users.noreply.github.com>
2014-05-08 09:54:30 +0000

    Amélioration de la lisibilité.

--- configs/database.inc.php
+++ configs/database.inc.php
@@ -2,6 +2,9 @@
 // -------------------------------------------------------------------------//
 // Informations sur la base de données
 //
+// Type de transaction à utiliser (mysql / mysqli / pdo)
+$inc['type'] = "pdo";
+//
 // Adresse de l'hôte de la base ou la chaine complète du DSN (Data Source Name)
 // Exemple de DSN pour PDO : mysql:host=127.0.0.1
 $inc['host'] = "mysql:host=127.0.0.1";
@@ -14,9 +17,6 @@ $inc['pass'] = "";
 //
 // Nom de la base
 $inc['name'] = "trancers_db";
-//
-// Type de transaction à utiliser (mysql / mysqli / pdo)
-$inc['type'] = "pdo";
 //
 // Préfixe des tables
 $inc['prefix'] = "tr";
