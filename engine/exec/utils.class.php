ee27dc8806f7f280da8c74240405808517eaa62c
Sébastien Villemain <39873273+Tr4ncer@users.noreply.github.com>
2014-06-09 08:25:10 +0000

    Optimisation.

--- engine/exec/utils.class.php
+++ engine/exec/utils.class.php
@@ -1,58 +1,73 @@
 <?php
 if (!defined("TR_ENGINE_INDEX")) {
-	require(".." . DIRECTORY_SEPARATOR . "core" . DIRECTORY_SEPARATOR . "secure.class.php");
-	Core_Secure::checkInstance();
+    require(".." . DIRECTORY_SEPARATOR . "core" . DIRECTORY_SEPARATOR . "secure.class.php");
+    Core_Secure::checkInstance();
 }
 
 /**
  * Fonction optimisé et utilitaire
- * 
+ *
  * @author Sébastien Villemain
  */
 class Exec_Utils {
-	
-	/**
-	 * Indique si une valeur appartient à un tableau
-	 * in_array optimized function
-	 * 
-	 * @author robin at robinnixon dot com
-	 * http://www.php.net/manual/fr/function.in-array.php#92460
-	 * 
-	 * @param $needle mixed
-	 * @param $haystack array
-	 * @return boolean si needle est trouvé
-	 */
-	public static function inArray($needle, $haystack) {
-		$top = sizeof($haystack) -1;
-		$bot = 0;
-		
-		while($top >= $bot) {
-			$p = floor(($top + $bot) / 2);
-			if ($haystack[$p] < $needle) $bot = $p + 1;
-			else if ($haystack[$p] > $needle) $top = $p - 1;
-			else return true;
-		}
-		return false;
-	}
-	
-	/**
-	 * Indique si une valeur appartient à un tableau (tableau multiple)
-	 * Tableau a dimension multiple
-	 * in_array multi array function
-	 * 
-	 * @param $needle mixed
-	 * @param $haystack array
-	 * @return boolean si needle est trouvé
-	 */
-	public static function inMultiArray($needle, $haystack) {
-		foreach ($haystack as $value) {
-			if (is_array($value)) { 
-				if (self::inMultiArray($needle, $value)) return true;
-			} else {
-				if ($value == $needle) return true;
-			}
-		}
-	}
-}
 
-?>
+    /**
+     * Indique si une valeur appartient à un tableau.
+     * in_array() optimized function.
+     *
+     * @author robin at robinnixon dot com
+     * http://www.php.net/manual/fr/function.in-array.php#92460
+     *
+     * @param string $needle
+     * @param array $haystack
+     * @return boolean
+     */
+    public static function inArray($needle, array $haystack) {
+        $rslt = false;
+        $top = sizeof($haystack) - 1;
+        $bot = 0;
+
+        while ($top >= $bot) {
+            $p = floor(($top + $bot) / 2);
+
+            if ($haystack[$p] < $needle) {
+                $bot = $p + 1;
+            } else if ($haystack[$p] > $needle) {
+                $top = $p - 1;
+            } else {
+                $rslt = true;
+                break;
+            }
+        }
+        return $rslt;
+    }
+
+    /**
+     * Indique si une valeur appartient à un tableau (tableau multiple).
+     * Tableau à dimension multiple.
+     * in_array()  multi array function.
+     *
+     * @param string $needle
+     * @param array $haystack
+     * @return boolean
+     */
+    public static function inMultiArray($needle, array $haystack) {
+        $rslt = false;
+
+        foreach ($haystack as $value) {
+            if (is_array($value)) {
+                if (self::inMultiArray($needle, $value)) {
+                    $rslt = true;
+                    break;
+                }
+            } else {
+                if ($value === $needle) {
+                    $rslt = true;
+                    break;
+                }
+            }
+        }
+        return $rslt;
+    }
+
+}
