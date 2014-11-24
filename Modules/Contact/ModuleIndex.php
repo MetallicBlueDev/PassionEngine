58c1cd27d926e9185a50a01f46157ed63fb45744
Sébastien Villemain <39873273+Tr4ncer@users.noreply.github.com>
2014-11-24 17:37:43 +0000

    use namespace.

--- Modules/Contact/ModuleIndex.php
+++ Modules/Contact/ModuleIndex.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace TREngine\Modules;
+namespace TREngine\Modules\Contact;
 
 require dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'engine' . DIRECTORY_SEPARATOR . 'SecurityCheck.php';
 
-class ModuleContactIndex extends ModuleModel {
+class ModuleIndex extends ModuleModel {
 
     public function display() {
         echo "Si vous souhaitez me contacter, le plus simple est de m'envoyer un e-mail (<a href=\"mailto:villemain.s_AT_gmail.com\">gmail.com</a>)"
