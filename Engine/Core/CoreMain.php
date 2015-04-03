9f5dd4297e06f931912f554531eded6e88234c4a
Sébastien Villemain <39873273+Tr4ncer@users.noreply.github.com>
2015-04-03 16:03:11 +0000

    Suppression de ExecAgent

--- Engine/Core/CoreMain.php
+++ Engine/Core/CoreMain.php
@@ -5,7 +5,6 @@ namespace TREngine\Engine\Core;
 use TREngine\Engine\Lib\LibBlock;
 use TREngine\Engine\Lib\LibModule;
 use TREngine\Engine\Lib\LibMakeStyle;
-use TREngine\Engine\Exec\ExecAgent;
 use TREngine\Engine\Exec\ExecMailer;
 use TREngine\Engine\Exec\ExecTimeMarker;
 use TREngine\Engine\Exec\ExecEntities;
