10efa26f14fa2aeac01d486eeb8322acdca642a6
Sébastien Villemain <39873273+Tr4ncer@users.noreply.github.com>
2015-05-05 17:20:28 +0000

    Structuration

--- Template/Default/module_management_setting.php
+++
@@ -1,4 +0,0 @@
-<?php
-include TR_ENGINE_INDEXDIR . DIRECTORY_SEPARATOR . "templates" . DIRECTORY_SEPARATOR . "default" . DIRECTORY_SEPARATOR . "module_management_bar.php";
-?>
-<?php echo $content; ?>
