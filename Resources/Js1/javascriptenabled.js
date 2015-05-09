583c817d62914ea896cdfd8b823b38f069734b7c
Sébastien Villemain <39873273+Tr4ncer@users.noreply.github.com>
2015-05-09 15:13:17 +0000

    Changement sur les ressources

---
+++ Resources/Js1/javascriptenabled.js
@@ -0,0 +1,14 @@
+/**
+ * Détection du javascript via cookie
+ *
+ * @author Sébastien Villemain
+ */
+function javascriptEnabled(name) {
+    if (navigator.cookieEnabled) {
+        var cookie_name = name;
+        if (document.cookie.indexOf(cookie_name + '=') < 0) {
+            document.cookie = cookie_name + '=' + escape(1);
+            document.location.reload()
+        }
+    }
+}
