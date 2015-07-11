d3713a892468c3c1324e0a21c900f4633a4d0d3d
Sébastien Villemain <39873273+Tr4ncer@users.noreply.github.com>
2015-07-11 21:57:36 +0000

    Nouvel entête !

--- Engine/Template/Defaultv2/index.php
+++ Engine/Template/Defaultv2/index.php
@@ -11,46 +11,34 @@
     <body>
         <div id="global">
             <div id="header">
-                <div style="display: none;"><h2><?php echo TREngine\Engine\Core\CoreMain::getInstance()->getDefaultSiteName() . " - " . TREngine\Engine\Core\CoreMain::getInstance()->getDefaultSiteSlogan(); ?></h2></div>
-                <div id="header_left"></div>
+                <div style="display: none;"><h2><?php echo TREngine\Engine\Core\CoreMain::getInstance()->getDefaultSiteName(); ?></h2></div>
+                <div id="header_left">
+                    <a href="index.php" title="<?php echo TREngine\Engine\Core\CoreMain::getInstance()->getDefaultSiteName(); ?>"></a>
+                    <h3><?php echo TREngine\Engine\Core\CoreMain::getInstance()->getDefaultSiteSlogan(); ?></h3>
+                </div>
                 <div id="header_middle"></div>
                 <div id="header_right"></div>
             </div>
-
             <div id="wrapper">
                 <div id="left">
-
                     <?php echo TREngine\Engine\Lib\LibBlock::getInstance()->getBlocksBySideName("right"); ?>
-
                 </div>
-
                 <div id="middle">
-
                     <?php echo TREngine\Engine\Core\CoreHtml::getInstance()->getLoader(); ?>
-
                     <div id="breadcrumb"><?php echo TREngine\Engine\Lib\LibBreadcrumb::getInstance()->getBreadcrumbTrail(" >> "); ?></div>
-
                     <div class="cleaner"></div>
-
                     <?php TREngine\Engine\Core\CoreLogger::displayMessages(); ?>
-
                     <?php echo TREngine\Engine\Lib\LibBlock::getInstance()->getBlocksBySideName("top"); ?>
-
                     <?php include TREngine\Engine\Lib\LibMakeStyle::getTemplateDir() . DIRECTORY_SEPARATOR . "module.php"; ?>
-
                     <?php echo TREngine\Engine\Lib\LibBlock::getInstance()->getBlocksBySideName("bottom"); ?>
-
                 </div>
             </div>
-
             <div id="footer">
                 <div style="padding: 50px;">
                     Page g&eacute;n&eacute;r&eacute;e en <?php echo TREngine\Engine\Exec\ExecTimeMarker::getMeasurement("main"); ?> ms.
                 </div>
             </div>
-
         </div>
-
         <?php echo TREngine\Engine\Core\CoreHtml::getInstance()->getMetaFooters(); ?>
     </body>
 </html>
