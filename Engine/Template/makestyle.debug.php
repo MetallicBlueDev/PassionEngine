6583e400c1b1e61854df4984144076989758c57f
Sébastien Villemain <39873273+Tr4ncer@users.noreply.github.com>
2015-07-27 16:11:47 +0000

    dev template

--- Engine/Template/makestyle.debug.php
+++ Engine/Template/makestyle.debug.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
     <h1><?php echo $errorMessageTitle; ?></h1>
-    <br /><br />
-    <?php if (!empty($errorMessage)) { ?>
-        Error details:<br />
+    <div>
+        <br /><br />
+        Error details:
+        <br />
         <ul>
             <?php foreach ($errorMessage as $value) { ?>
                 <li><?php echo $value; ?></li>
             <?php } ?>
         </ul>
-    <?php } ?>
+    </div>
 </html>
