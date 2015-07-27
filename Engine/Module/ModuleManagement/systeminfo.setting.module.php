d18d8d5d34503c74b301a957065f2e398418a991
Sébastien Villemain <39873273+Tr4ncer@users.noreply.github.com>
2015-07-27 09:42:15 +0000

    Compatiblité HTML5

--- Engine/Module/ModuleManagement/systeminfo.setting.module.php
+++ Engine/Module/ModuleManagement/systeminfo.setting.module.php
@@ -70,7 +70,8 @@ class Module_Management_Systeminfo extends ModuleModel {
         preg_match_all('#<body[^>]*>(.*)</body>#siU', $phpinfo, $output);
         $output = preg_replace('#<table#', '<table class="table"', $output[1][0]);
         $output = preg_replace('#(\w),(\w)#', '\1, \2', $output);
-        $output = preg_replace('#border="0" cellpadding="3" width="600"#', 'border="0" cellspacing="1" cellpadding="4" width="95%"', $output);
+        // Incompatible HTML5
+//        $output = preg_replace('#border="0" cellpadding="3" width="600"#', 'border="0" cellspacing="1" cellpadding="4" width="95%"', $output);
         $output = preg_replace('#<hr />#', '', $output);
         $output = str_replace('<div class="center">', '', $output);
         $output = str_replace('</div>', '', $output);
