c48465a240c6dc0cebf97fcf4b749fdf5e2c8961
Sébastien Villemain <39873273+Tr4ncer@users.noreply.github.com>
2015-09-04 17:55:14 +0000

    nettoyage

--- Engine/Template/MetallicBlueSky/main.php
+++ Engine/Template/MetallicBlueSky/main.php
@@ -10,93 +10,91 @@
         <div id="metallic_blue_sky">
             <div id="metallic_blue_sky_background"></div>
             <div id="metallic_blue_sky_screen">
-                <div id="metallic_blue_sky_global">
-                    <header id="header">
-                        <div style="display: none;"><?php echo TREngine\Engine\Core\CoreMain::getInstance()->getDefaultSiteName(); ?></div>
-                        <div id="header_left">
-                            <div class="header_wire_harness wire_harness_left">
-                                <div class="wire_harness_object wire_harness_anim1"></div>
-                                <div class="wire_harness_object wire_harness_anim2"></div>
-                                <div class="wire_harness_object wire_harness_anim1"></div>
-                            </div>
-                            <a href="index.php" title="<?php echo TREngine\Engine\Core\CoreMain::getInstance()->getDefaultSiteName(); ?>"></a>
-                            <h3><?php echo TREngine\Engine\Core\CoreMain::getInstance()->getDefaultSiteSlogan(); ?></h3>
+                <header id="header">
+                    <div style="display: none;"><?php echo TREngine\Engine\Core\CoreMain::getInstance()->getDefaultSiteName(); ?></div>
+                    <div id="header_left">
+                        <div class="header_wire_harness wire_harness_left">
+                            <div class="wire_harness_object wire_harness_anim1"></div>
+                            <div class="wire_harness_object wire_harness_anim2"></div>
+                            <div class="wire_harness_object wire_harness_anim1"></div>
                         </div>
-                        <div id="header_middle"></div>
-                        <div id="header_right">
-                            <div class="header_wire_harness wire_harness_right">
-                                <div class="wire_harness_object wire_harness_anim1"></div>
-                                <div class="wire_harness_object wire_harness_anim2"></div>
-                                <div class="wire_harness_object wire_harness_anim1"></div>
-                            </div>
+                        <a href="index.php" title="<?php echo TREngine\Engine\Core\CoreMain::getInstance()->getDefaultSiteName(); ?>"></a>
+                        <h3><?php echo TREngine\Engine\Core\CoreMain::getInstance()->getDefaultSiteSlogan(); ?></h3>
+                    </div>
+                    <div id="header_middle"></div>
+                    <div id="header_right">
+                        <div class="header_wire_harness wire_harness_right">
+                            <div class="wire_harness_object wire_harness_anim1"></div>
+                            <div class="wire_harness_object wire_harness_anim2"></div>
+                            <div class="wire_harness_object wire_harness_anim1"></div>
                         </div>
-                    </header>
-                    <div id="wrapper">
-                        <div id="wrapper_leftfix">
-                            <div id="wrapper_rightfix">
-                                <div id="wrapper_left">
-                                    <?php echo TREngine\Engine\Lib\LibBlock::getInstance()->getBlocksBySideName("left"); ?>
-                                </div>
-                                <div id="wrapper_middlefix">
-                                    <div id="wrapper_middle">
-                                        <?php echo TREngine\Engine\Core\CoreHtml::getInstance()->getLoader(); ?>
-                                        <?php echo TREngine\Engine\Lib\LibBreadcrumb::getInstance()->getBreadcrumbTrail(); ?>
-                                        <div class="spacer"></div>
-                                        <?php TREngine\Engine\Core\CoreLogger::displayMessages(); ?>
-                                        <?php echo TREngine\Engine\Lib\LibBlock::getInstance()->getBlocksBySideName("top"); ?>
-                                        <div>
-                                            <?php echo TREngine\Engine\Lib\LibBlock::getInstance()->getBlocksBySideName("moduleleft"); ?>
-                                        </div>
-                                        <div>
-                                            <?php echo TREngine\Engine\Lib\LibBlock::getInstance()->getBlocksBySideName("moduletop"); ?>
-                                        </div>
-                                        <div id="module_top">
-                                            <div id="module_top_left"></div>
-                                            <div id="module_top_middle"></div>
-                                            <div id="module_top_right"></div>
-                                        </div>
-                                        <div id="module_content_leftfix">
-                                            <div id="module_content_rightfix">
-                                                <div id="module_content_middle">
-                                                    <?php echo TREngine\Engine\Lib\LibModule::getInstance()->getModule(); ?>
-                                                </div>
+                    </div>
+                </header>
+                <div id="wrapper">
+                    <div id="wrapper_leftfix">
+                        <div id="wrapper_rightfix">
+                            <div id="wrapper_left">
+                                <?php echo TREngine\Engine\Lib\LibBlock::getInstance()->getBlocksBySideName("left"); ?>
+                            </div>
+                            <div id="wrapper_middlefix">
+                                <div id="wrapper_middle">
+                                    <?php echo TREngine\Engine\Core\CoreHtml::getInstance()->getLoader(); ?>
+                                    <?php echo TREngine\Engine\Lib\LibBreadcrumb::getInstance()->getBreadcrumbTrail(); ?>
+                                    <div class="spacer"></div>
+                                    <?php TREngine\Engine\Core\CoreLogger::displayMessages(); ?>
+                                    <?php echo TREngine\Engine\Lib\LibBlock::getInstance()->getBlocksBySideName("top"); ?>
+                                    <div>
+                                        <?php echo TREngine\Engine\Lib\LibBlock::getInstance()->getBlocksBySideName("moduleleft"); ?>
+                                    </div>
+                                    <div>
+                                        <?php echo TREngine\Engine\Lib\LibBlock::getInstance()->getBlocksBySideName("moduletop"); ?>
+                                    </div>
+                                    <div id="module_top">
+                                        <div id="module_top_left"></div>
+                                        <div id="module_top_middle"></div>
+                                        <div id="module_top_right"></div>
+                                    </div>
+                                    <div id="module_content_leftfix">
+                                        <div id="module_content_rightfix">
+                                            <div id="module_content_middle">
+                                                <?php echo TREngine\Engine\Lib\LibModule::getInstance()->getModule(); ?>
                                             </div>
                                         </div>
-                                        <div id="module_bottom">
-                                            <div id="module_bottom_left"></div>
-                                            <div id="module_bottom_middle"></div>
-                                            <div id="module_bottom_right"></div>
-                                        </div>
-                                        <div>
-                                            <?php echo TREngine\Engine\Lib\LibBlock::getInstance()->getBlocksBySideName("modulebottom"); ?>
-                                        </div>
-                                        <div>
-                                            <?php echo TREngine\Engine\Lib\LibBlock::getInstance()->getBlocksBySideName("moduleright"); ?>
-                                        </div>
-                                        <?php echo TREngine\Engine\Lib\LibBlock::getInstance()->getBlocksBySideName("bottom"); ?>
                                     </div>
+                                    <div id="module_bottom">
+                                        <div id="module_bottom_left"></div>
+                                        <div id="module_bottom_middle"></div>
+                                        <div id="module_bottom_right"></div>
+                                    </div>
+                                    <div>
+                                        <?php echo TREngine\Engine\Lib\LibBlock::getInstance()->getBlocksBySideName("modulebottom"); ?>
+                                    </div>
+                                    <div>
+                                        <?php echo TREngine\Engine\Lib\LibBlock::getInstance()->getBlocksBySideName("moduleright"); ?>
+                                    </div>
+                                    <?php echo TREngine\Engine\Lib\LibBlock::getInstance()->getBlocksBySideName("bottom"); ?>
                                 </div>
-                                <div id="wrapper_right_compatibility">
-                                    <div class="wrapper_quick_lighting"></div>
-                                    <div id="wrapper_right">
-                                        <?php echo TREngine\Engine\Lib\LibBlock::getInstance()->getBlocksBySideName("right"); ?>
-                                    </div>
+                            </div>
+                            <div id="wrapper_right_compatibility">
+                                <div class="wrapper_quick_lighting"></div>
+                                <div id="wrapper_right">
+                                    <?php echo TREngine\Engine\Lib\LibBlock::getInstance()->getBlocksBySideName("right"); ?>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <footer id="footer">
-                        <div id="footer_left"></div>
-                        <div id="footer_middle">
-                            <div style="padding: 50px;">
-                                Page g&eacute;n&eacute;r&eacute;e en <?php echo TREngine\Engine\Exec\ExecTimeMarker::getMeasurement("main"); ?> ms.
-                            </div>
-                        </div>
-                        <div id="footer_right"></div>
-                    </footer>
                 </div>
-                <?php echo TREngine\Engine\Core\CoreHtml::getInstance()->getMetaFooters(); ?>
+                <footer id="footer">
+                    <div id="footer_left"></div>
+                    <div id="footer_middle">
+                        <div style="padding: 50px;">
+                            Page g&eacute;n&eacute;r&eacute;e en <?php echo TREngine\Engine\Exec\ExecTimeMarker::getMeasurement("main"); ?> ms.
+                        </div>
+                    </div>
+                    <div id="footer_right"></div>
+                </footer>
             </div>
+            <?php echo TREngine\Engine\Core\CoreHtml::getInstance()->getMetaFooters(); ?>
         </div>
     </body>
 </html>
