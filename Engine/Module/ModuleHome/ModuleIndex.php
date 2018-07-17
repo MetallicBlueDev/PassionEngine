69aff053b3ad9b1e3266947dc53694ed4f709644
Tr4ncer <villemain.s@gmail.com>
2018-07-17 14:33:47 +0200

    Affichage

--- Engine/Module/ModuleHome/ModuleIndex.php
+++ Engine/Module/ModuleHome/ModuleIndex.php
@@ -7,23 +7,18 @@ use TREngine\Engine\Module\ModuleModel;
 class ModuleIndex extends ModuleModel
 {
 
-    public function display()
+    public function display(): void
     {
         ?>
         <div class="title">
-            <span>Bonjour, bienvenue sur <a href="index.php">Trancer-Studio.net</a>.</span>
+            <span>Bonjour, bienvenue sur <a href="index.php">le moteur TR ENGINE</a>.</span>
         </div>
         <br /><br />
         <div class="description" style="width: 70%;">
-            <span>Le site est un peu vide, mais il va se remplir petit &#224; petit...</span>
-            <br /><br />A venir : des textes, de la documentation, des exemples, des images relatif &#224; mes projets.
+            <span>Ceci est un exemple d'affichage.</span>
+            <br /><br />
         </div>
 
         <?php
-    }
-
-    public function setting()
-    {
-        return "Pas de setting...";
     }
 }
